cfa3472c33797a7efd25613f88c1ba6d14f50ce8
Kimran <kimran_06@mail.ru>
2014-05-30 01:19:08 +0400

    working start

---
+++ Model/NameSurname.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GSKCrossplatformService.Model
+{
+    public class NameSurname
+    {
+    }
+}
